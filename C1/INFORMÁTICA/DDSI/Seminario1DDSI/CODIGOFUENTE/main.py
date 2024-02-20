4b3ec3b6bfe7c4447d3c7d64cc7154ade5f0a8d6
Quintin Mesa <quintinmr12@gmail.com>
2024-02-20 21:12:56 +0100

    DDSI

--- C1/INFORMÁTICA/DDSI/Seminario1DDSI/CODIGOFUENTE/main.py
+++
@@ -1,9 +0,0 @@
-
-import controlador
-
-# INSTALAR, SI NO ESTÁ INSTALADA, LA FUENTE LEXEND ANTES DE EJECUTAR!!
-
-controlador = controlador.Controlador()
-controlador.showMenu1() 
-controlador.cerrarConexion()
-
